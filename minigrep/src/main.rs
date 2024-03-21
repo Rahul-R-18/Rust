da4bdee712e64a000eafd78b563fd28e81b8b4f8
Rahul-R-24 <rahulram2204@gmail.com>
2024-03-20 20:52:40 -0400

    3/20

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,15 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|e| {
-        println!("Problem parsing arguments: {e}");
+        eprintln!("Problem parsing arguments: {e}");
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
-
+    
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
